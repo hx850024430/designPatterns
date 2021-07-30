a1f0ede9c6207fdb55799b001302a47bb3076478
hx <hx850024430@163.com>
2021-07-30 21:33:44 +0800

    简单工厂模式

--- src/com/hx/designPatterns/Factory/simple/SimpleFactory.java
+++ src/com/hx/designPatterns/Factory/simple/SimpleFactory.java
@@ -1,4 +1,13 @@
 package com.hx.designPatterns.Factory.simple;
 
 public class SimpleFactory {
+    public Animal getAnimal(String name){
+        if ("Dog".equals(name)){
+            return new Dog("dog");
+        }else if ("Cat".equals(name)){
+            return new Cat("cat");
+        }else {
+            return new Pig("pig");
+        }
+    }
 }
